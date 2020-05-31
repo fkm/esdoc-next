f7fcc82b3d508bf20bde5001dbbe67335dedb3b7
Daniel Ruf <mac1@daniel-ruf.de>
2020-05-31 19:45:08 +0200

    Run eslint only on Node 8 and later

--- script/test-ci.js
+++ script/test-ci.js
@@ -1,6 +1,10 @@
 #!/usr/bin/env node
 const sh = require('./sh');
 
-sh.exec('./script/eslint.js');
+const NODE_MAJOR_VERSION = process.version.match(/^v(\d+)/)[1];
+
+if (NODE_MAJOR_VERSION >= 8) {
+  sh.exec('./script/eslint.js');
+}
 sh.exec('./script/test.js --coverage');
 sh.exec('./node_modules/.bin/codecov');
